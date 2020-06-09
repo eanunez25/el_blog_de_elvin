3fbb69cccd30751c97f0716b27be1ebaeb7a0619
Elvin Nunez <eanunez25@gmail.com>
2020-06-08 21:51:03 -0500

    complete tagging feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
@@ -11,4 +12,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:author_name, :body)
   end
+
 end
